28829d9b2ce249e0ae3fbd9ffbd413a03707f6d8
Rakesh Morane <rakesh.morane@gmail.com>
2026-06-23 15:04:42 +0530

    change

--- src/hello.py
+++ src/hello.py
@@ -1,3 +1,8 @@
 print("hello world")
 print("how are you")
 print("hello how are rakeshg")
+print (jflsjdflfljsdlkjflkjsdfkajfkjsdlfkjsdlkfjldsjflkdsflkjk
+       sdfjodsjfiosjofjs
+       sfsdfsjflsdjflsjfljslf
+       slfjdslfjlsdkfjlsd
+       slfjjlfjler)
